71d56158fe128e2de3afea3ca54e40d0f2b43f41
James Polley <jmspolley@gmail.com>
2021-06-14 15:28:10 -0500

    Implement _identify_command_type

--- Parser.py
+++ Parser.py
@@ -5,16 +5,19 @@ import re
 
 class Parser:
     # Command Types
-    NOT_A_COMMAND = 0
-    C_ARITHMETIC = 1
-    C_PUSH = 2
-    C_POP = 3
-    C_LABEL = 4
-    C_GOTO = 5
-    C_IF = 6
-    C_FUNCTION = 7
-    C_RETURN = 8
-    C_CALL = 9
+    C_ARITHMETIC  = 1
+    C_PUSH        = 2
+    C_POP         = 3
+    C_LABEL       = 4
+    C_GOTO        = 5
+    C_IF          = 6
+    C_FUNCTION    = 7
+    C_RETURN      = 8
+    C_CALL        = 9
+
+    # Arithmetic / Logical Commands
+    ARITHMETIC = [
+        "add", "sub", "neg", "eq", "gt", "lt", "and", "or", "not"]
 
     # Arguement 1
     #   to do
@@ -50,7 +53,7 @@ class Parser:
         parts = Parser._format_line(line_to_parse)
         if not parts:
             return False
-        command_type = Parser._identify_command_type(parts[0])
+        command_type = Parser._identify_command_type(parts)
         arg_1 = Parser._identify_arg_1(command_type, parts[1])
         arg_2 = Parser._identify_arg_2(command_type, parts[2])
         return command_type, arg_1, arg_2
@@ -67,8 +70,20 @@ class Parser:
         return tuple(parts[:3])
     
     @staticmethod
-    def _identify_command_type(command):
-        pass
+    def _identify_command_type(parts):
+        command = parts[0]
+        if command == "push": return Parser.C_PUSH
+        if command == "pop": return Parser.C_POP
+        if command == "label": return Parser.C_LABEL
+        if command == "goto": return Parser.C_GOTO
+        if command == "if-goto": return Parser.C_IF
+        if command == "function": return Parser.C_FUNCTION
+        if command == "return": return Parser.C_RETURN
+        if command == "call": return Parser.C_CALL
+        if command in Parser.ARITHMETIC: return Parser.C_ARITHMETIC
+        raise Exception("'{}' does not match any valid command"\
+            .format(command))
+        return
     
     @staticmethod
     def _identify_arg_1(command_type, arg_1):
@@ -85,7 +100,8 @@ print(p.file)
 print(p.has_next())
 p.advance()
 
+print(p.ARITHMETIC)
+
 print(p._format_line("    asd asdf asdf asdf asdf // asdgasg\n"))
 print(p._format_line("    as"))
-print(p._format_line(""))
-print(p._parse_line("    asd asdf asdf asdf asdf // asdgasg\n"))
+print(p._parse_line("Push asdf 3"))
