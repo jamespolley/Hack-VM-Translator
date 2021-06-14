b6993c96b7af5c6a64ba92b0d5e4d2d4149888b7
James Polley <jmspolley@gmail.com>
2021-06-14 14:33:08 -0500

    Continue Parser implementation, fix VMTranslator

--- Parser.py
+++ Parser.py
@@ -1,6 +1,8 @@
 # Handle parsing of input file, break each command into its lexical
 # componenets. Ignores whitespace and comments.
 
+import re
+
 class Parser:
     # Command Types
     NOT_A_COMMAND = 0
@@ -20,6 +22,9 @@ class Parser:
     # Arguement 2
     #   to do
 
+    RE_COMMENT = r"//.*\n"
+    RE_WHITESPACE = r"\s+"
+
     def __init__(self, vm_file):
         with open(vm_file, "r") as f:
             self.file = f.readlines()
@@ -28,34 +33,59 @@ class Parser:
         self.current_line = None
     
     def has_next(self):
-        return self.current_idx <= self.last_idx
+        return self.current_idx < self.last_idx
     
     def advance(self):
         self.current_idx += 1
+        self.current_line = self.file[self.current_idx]
     
-    def get_current(self):
+    def get_current_line(self):
         return self.file[self.current_idx]
     
-    def parse(self):
-        command_type = 123 # temp
-        arg_1 = 456 # temp
-        arg_2 = 789 # temp
+    def parse_current_line(self):
+        return Parser._parse_line(self.current_line)
+    
+    @staticmethod
+    def _parse_line(line_to_parse):
+        parts = Parser._format_line(line_to_parse)
+        if not parts:
+            return False
+        command_type = Parser._identify_command_type(parts[0])
+        arg_1 = Parser._identify_arg_1(command_type, parts[1])
+        arg_2 = Parser._identify_arg_2(command_type, parts[2])
         return command_type, arg_1, arg_2
     
-    def get_command_type(self):
+    @staticmethod
+    def _format_line(line_to_format):
+        """Return tuple of formatted line's component parts, filled in with None if part is absent."""
+        line = re.sub(Parser.RE_COMMENT, "", line_to_format).strip()
+        parts = re.split(Parser.RE_WHITESPACE, line)
+        if not parts[0]:
+            return False
+        while len(parts) < 3:
+            parts.append(None)
+        return tuple(parts[:3])
+    
+    @staticmethod
+    def _identify_command_type(command):
         pass
-
-    def get_arg_1(self):
+    
+    @staticmethod
+    def _identify_arg_1(command_type, arg_1):
         pass
-
-    def get_arg_2(self):
+    
+    @staticmethod
+    def _identify_arg_2(command_type, arg_2):
         pass
 
 
 # tests
 p = Parser("assets\MemoryAccess\BasicTest\BasicTest.vm")
 print(p.file)
-print(p.parse())
 print(p.has_next())
 p.advance()
-print(p.get_current())
+
+print(p._format_line("    asd asdf asdf asdf asdf // asdgasg\n"))
+print(p._format_line("    as"))
+print(p._format_line(""))
+print(p._parse_line("    asd asdf asdf asdf asdf // asdgasg\n"))
--- VMTranslator.py
+++ VMTranslator.py
@@ -3,3 +3,21 @@
 # 
 # Main class. Construct Parser and CodeWriter, go through file, parse
 # each line, generate code.
+
+from Parser import Parser
+
+class VMTranslator:
+    def __init__(self, vm_file):
+        self.parser = Parser(vm_file)
+        # to do - add CodeWriter
+    
+    def translate(self):
+        while self.parser.has_next():
+            self.parser.advance()
+            print(self.parser.parse_current_line())
+            
+    
+
+# tests
+vmt = VMTranslator("assets\MemoryAccess\BasicTest\BasicTest.vm")
+vmt.translate()
